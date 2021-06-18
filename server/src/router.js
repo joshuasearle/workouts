794d7cddc8bf1066544ceea44aba446e0dbc6dd9
joshuasearle <joshuasearle@gmail.com>
2021-06-18 21:16:48 +1000

    Add user routes to router

--- server/src/router.js
+++ server/src/router.js
@@ -2,4 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+router.post('/create-user', require('./controllers/createUser'));
+router.post('/login', require('./controllers/login'));
+
 module.exports = router;
