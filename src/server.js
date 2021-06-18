845706aa2ad49434e5f8e083b8b99be3dca9c755
Josh Searle <joshryansearle@gmail.com>
2021-06-18 16:14:11 +1000

    Change folder name

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+require('./models/connection');
+
 dotenv.config();
 const defaultPort = 5000;
 const port = process.env.PORT || defaultPort;
