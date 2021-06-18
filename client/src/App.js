b4b47bc84f8de2b17c83ef65aed727e25083f9ea
Josh Searle <joshryansearle@gmail.com>
2021-06-18 16:52:28 +1000

    Query api

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,14 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function App() {
-  return <div>App</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const url = `http://${process.env.REACT_APP_API_PATH}/users`;
+    axios
+      .get(url)
+      .then((response) => setUsers(response.data))
+      .catch((err) => console.log(err));
+  });
+  return <div>{users && users.map((user) => user.username)}</div>;
 }
