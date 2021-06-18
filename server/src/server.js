a1c88f73846b7dd426531a587613ae13fa03e1a3
Josh Searle <joshryansearle@gmail.com>
2021-06-18 18:44:46 +1000

    Add add user route

--- server/src/server.js
+++ server/src/server.js
@@ -9,14 +9,34 @@ const defaultPort = 5000;
 const port = process.env.PORT || defaultPort;
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use(express.json());
+
 app.get('/health', (req, res) => {
   res.status(200).send();
 });
 
 app.get('/users', async (req, res) => {
   const users = await User.find({});
-  console.log(users);
+  // console.log(users);
   res.send(users);
+});
+
+app.post('/users', async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  await user.save();
+  res.status(201).send();
 });
 
 app.listen(port, () => {
