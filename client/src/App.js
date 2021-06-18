9930c6f8f704c30fd557583101a42e42e4da3fa5
Josh Searle <joshryansearle@gmail.com>
2021-06-18 18:44:34 +1000

    Add add user form

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,47 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    const url = `http://${process.env.REACT_APP_API_PATH}/users`;
+    const url = 'http://localhost:5000/users';
     axios
       .get(url)
       .then((response) => setUsers(response.data))
       .catch((err) => console.log(err));
-  });
-  return <div>{users && users.map((user) => user.username)}</div>;
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const url = 'http://localhost:5000/users';
+    axios.post(url, {
+      username,
+      password,
+    });
+  };
+
+  return (
+    <div>
+      <div>{users && users.map((user) => user.username).join(', ')}</div>
+      <label>Username</label>
+      <br />
+      <input
+        type='text'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <br />
+      <label>Password</label>
+      <br />
+      <input
+        type='text'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
 }
