601112ebb9d3a750f8776d7fbe1b291ba1228562
Josh Searle <joshryansearle@gmail.com>
2021-06-18 16:23:47 +1000

    Add users route to api

--- src/server.js
+++ src/server.js
@@ -2,14 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 require('./models/connection');
+const User = require('./models/user');
 
 dotenv.config();
 const defaultPort = 5000;
 const port = process.env.PORT || defaultPort;
 const app = express();
 
-app.use('/', (req, res) => {
+app.get('/health', (req, res) => {
   res.status(200).send();
+});
+
+app.get('/users', async (req, res) => {
+  const users = await User.find({});
+  console.log(users);
+  res.send(users);
 });
 
 app.listen(port, () => {
