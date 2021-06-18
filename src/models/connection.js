a3aedc0a1b5f7239de5bf49a5dc1f73f84293407
Josh Searle <joshryansearle@gmail.com>
2021-06-18 16:13:39 +1000

    Connect to database

---
+++ src/models/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Database connected'));
