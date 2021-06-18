9e7aedd7c62a325ae51005039f40d515aed9e107
joshuasearle <joshuasearle@gmail.com>
2021-06-18 21:31:05 +1000

    Add token authentication

--- server/src/auth.js
+++ server/src/auth.js
@@ -8,3 +8,16 @@ function generateAccessToken(username) {
     expiresIn: '1000h',
   });
 }
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.status(401).send();
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).send();
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = { generateAccessToken, authenticateToken };
