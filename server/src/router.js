de09bc44541b1608a70a0f79c544ad5f5148492a
joshuasearle <joshryansearle@gmail.com>
2021-06-19 20:20:39 +1000

    Add workout routes and checkers

--- server/src/router.js
+++ server/src/router.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+// Checkers
+const checkers = require('./controllers/checkers');
+const { checkExerciseId, checkWorkoutId } = checkers;
+
 // User routes
 const userControllers = require('./controllers/userControllers');
 const { createUser, loginUser } = userControllers;
@@ -20,8 +24,32 @@ const { createExercise, removeExercise, updateExercise, duplicateExercise } =
   exerciseControllers;
 
 router.post('/exercises', createExercise);
-router.put('/exercises', updateExercise);
-router.delete('/exercises', removeExercise);
-router.post('/duplicate-exercise', duplicateExercise);
+router.put('/exercises', checkExerciseId, updateExercise);
+router.delete('/exercises', checkExerciseId, removeExercise);
+router.post('/duplicate-exercise', checkExerciseId, duplicateExercise);
+
+// Workshop routes
+const workoutControllers = require('./controllers/workoutControllers');
+const {
+  createWorkout,
+  removeWorkout,
+  addExerciseToWorkout,
+  removeExerciseFromWorkout,
+} = workoutControllers;
+
+router.post('/workouts', createWorkout);
+router.delete('/workouts', checkWorkoutId, removeWorkout);
+router.post(
+  '/workouts/add-exercise',
+  checkExerciseId,
+  checkWorkoutId,
+  addExerciseToWorkout
+);
+router.post(
+  '/workouts/remove-exercise',
+  checkWorkoutId,
+  checkExerciseId,
+  removeExerciseFromWorkout
+);
 
 module.exports = router;
