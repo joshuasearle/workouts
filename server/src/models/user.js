64b55c84734a42c0c9f849b1522051889364afeb
joshuasearle <joshuasearle@gmail.com>
2021-06-19 13:57:42 +1000

    Add exercises field to user

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   workouts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workout' }],
+  exercises: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' }],
 });
 
 const User = mongoose.model('User', userSchema);
