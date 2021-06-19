188426b4dce72319f262c1033ecedd10cac24879
joshuasearle <joshuasearle@gmail.com>
2021-06-19 13:59:01 +1000

    Add create exercise to router

--- server/src/router.js
+++ server/src/router.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
+const { authenticateToken } = require('./auth');
 
 router.post('/create-user', require('./controllers/createUser'));
 router.post('/login', require('./controllers/login'));
+
+router.post(
+  '/exercise',
+  authenticateToken,
+  require('./controllers/createExercise')
+);
 
 module.exports = router;
