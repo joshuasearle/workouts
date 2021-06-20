1fb4113ff1fe68afe2e3f9c67d8e7e987e57f336
joshuasearle <joshryansearle@gmail.com>
2021-06-20 16:55:46 +1000

    Fix checker bug

--- server/src/controllers/checkers.js
+++ server/src/controllers/checkers.js
@@ -1,12 +1,14 @@
 function checkExerciseId(req, res, next) {
-  if (!req.user.exercises.includes(exerciseId)) {
+  const { exerciseId } = req.body;
+  if (!req.user.exercises.map((e) => e._id).includes(exerciseId)) {
     return res.status(400).send('No exercise with this id');
   }
   next();
 }
 
 function checkWorkoutId(req, res, next) {
-  if (!req.user.workouts.includes(workoutId)) {
+  const { workoutId } = req.body;
+  if (!req.user.workouts.map((w) => w._id).includes(workoutId)) {
     return res.status(400).send('No workout with this id');
   }
   next();
