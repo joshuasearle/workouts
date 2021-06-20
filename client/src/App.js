54dcc57830f8709e7f2482f6cbae089d8083c6f9
joshuasearle <joshryansearle@gmail.com>
2021-06-20 16:22:50 +1000

    Add pages and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,34 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import useToken from './hooks/useToken';
+
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const url = 'http://localhost:5000/users';
-    axios
-      .get(url)
-      .then((response) => setUsers(response.data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    const url = 'http://localhost:5000/users';
-    axios.post(url, {
-      username,
-      password,
-    });
-  };
+  const [token, setToken] = useToken();
 
   return (
-    <div>
-      <div>{users && users.map((user) => user.username).join(', ')}</div>
-      <label>Username</label>
-      <br />
-      <input
-        type='text'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <br />
-      <label>Password</label>
-      <br />
-      <input
-        type='text'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      <button onClick={handleClick}>Submit</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/login'>
+          {!!token && <Redirect to='/' />}
+          <LoginPage token={token} setToken={setToken} />
+        </Route>
+        <Route exact path='/signup'>
+          {!!token && <Redirect to='/' />}
+          <SignUpPage token={token} setToken={setToken} />
+        </Route>
+        <Route exact path='/'>
+          <HomePage token={token} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
