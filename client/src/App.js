40f5e8fef7657543271b5a47c48c6338bef009ae
joshuasearle <joshryansearle@gmail.com>
2021-06-21 17:35:04 +1000

    Add user data logic to app

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,38 @@ import {
 } from 'react-router-dom';
 
 import useToken from './hooks/useToken';
+import useUserData from './hooks/useUserData';
 
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
+import { useEffect } from 'react';
 
 export default function App() {
   const [token, setToken] = useToken();
+  const [userData, updateUserData] = useUserData();
+
+  useEffect(() => {
+    try {
+      updateUserData();
+    } catch (e) {
+      setToken(null);
+    }
+  }, []);
 
   return (
     <Router>
       <Switch>
         <Route exact path='/login'>
           {!!token && <Redirect to='/' />}
-          <LoginPage token={token} setToken={setToken} />
+          <LoginPage setToken={setToken} updateUserData={updateUserData} />
         </Route>
         <Route exact path='/signup'>
           {!!token && <Redirect to='/' />}
-          <SignUpPage token={token} setToken={setToken} />
+          <SignUpPage setToken={setToken} updateUserData={updateUserData} />
         </Route>
-        <Route exact path='/'>
-          <HomePage token={token} />
+        <Route exact path='/' userData={userData}>
+          <HomePage userData={userData} />
         </Route>
       </Switch>
     </Router>
---
+++ client/src/hooks/useUserData.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import getUserData from '../api/getUserData';
+
+export default function useUserData() {
+  const [userData, setUserData] = useState(null);
+
+  async function updateUserData(token) {
+    if (!localStorage.getItem('workoutsJWT')) return;
+    try {
+      const userData = await getUserData();
+      setUserData(userData);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return [userData, updateUserData];
+}
