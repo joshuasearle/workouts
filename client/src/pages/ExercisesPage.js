0f1189c98569b377c6eee135d09f7d5dddd68c83
joshuasearle <joshryansearle@gmail.com>
2021-06-22 12:22:30 +1000

    Fix exercises list styling

--- client/src/pages/ExercisesPage.js
+++ client/src/pages/ExercisesPage.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import deleteExercise from '../api/deleteExercise';
 
 export default function ExercisesPage({ userData, updateUserData }) {
@@ -13,30 +14,37 @@ export default function ExercisesPage({ userData, updateUserData }) {
     <>
       <h1>Exercises</h1>
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Set Count</th>
-          <th>Rep Count</th>
-          <th>Rest Period</th>
-        </tr>
-        {userData.exercises.map((exercise) => (
-          <tr key={exercise.title}>
-            <td>{exercise.title}</td>
-            <td>{exercise.description}</td>
-            <td>{exercise.setCount}</td>
-            <td>{exercise.repCount}</td>
-            <td>{exercise.secondsBreak}s</td>
-            <td>
-              <button>Edit</button>
-            </td>
-            <td>
-              <button onClick={onDelete(exercise._id)}>Delete</button>
-            </td>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Set Count</th>
+            <th>Rep Count</th>
+            <th>Rest Period</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {userData.exercises.map((exercise) => (
+            <tr key={exercise.title}>
+              <td>{exercise.title}</td>
+              <td>{exercise.description}</td>
+              <td>{exercise.setCount}</td>
+              <td>{exercise.repCount}</td>
+              <td>{exercise.secondsBreak}s</td>
+              <td>
+                <button>Edit</button>
+              </td>
+              <td>
+                <button>Duplicate</button>
+              </td>
+              <td>
+                <button onClick={onDelete(exercise._id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-      <button>Add New Exercise</button>
+      <Link to='/add-exercise'>Add New Exercise</Link>
     </>
   );
 }
